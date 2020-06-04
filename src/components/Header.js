e279c4eaea55c750521d94d505f33c2b421ada7b
unknown <opokudavid446@gmail.com>
2020-06-04 12:56:36 +0100

    Completed header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,24 @@
 import React from "react";
 
-const Header = () => <div> Header </div>;
+const Header = () => (
+  <div className="header">
+    <div className="logo-section">
+      <div className="logo">
+        <div />
+        <span className="mask" />
+      </div>
+      <span>
+        <span className="title" > Smile</span>
+        <span>.</span>
+      </span>
+    </div>
+    <div className="menu-section">
+      <h4> About </h4>
+      <h4> Blog </h4>
+      <h4> Help </h4>
+      <h4 className="account"> Account </h4>
+    </div>
+  </div>
+);
 
 export default Header;
---
+++ src/svg/Svg.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+export const Play = ({ color, size }) => (
+  <svg
+    height={size}
+    version="1.1"
+    viewBox="0 0 13 14"
+    width="13px"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <title />
+    <desc />
+    <defs />
+    <g
+      fill="none"
+      fill-rule="evenodd"
+      id="Page-1"
+      stroke="none"
+      stroke-width="1"
+    >
+      <g
+        fill={color}
+        id="Icons-AV"
+        transform="translate(-88.000000, -88.000000)"
+      >
+        <g id="play-arrow" transform="translate(88.500000, 88.000000)">
+          <path d="M0,0 L0,14 L11,7 L0,0 Z" id="Shape" />
+        </g>
+      </g>
+    </g>
+  </svg>
+);
