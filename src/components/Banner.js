96719e039e97b22667c6ccfa7d755728abddfffa
unknown <opokudavid446@gmail.com>
2020-06-04 13:06:34 +0100

    Modify header

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,5 +1,10 @@
 import React from "react";
+import Img from "../images/img.jpg";
 
-const Banner = () => <div> Banner </div>;
+const Banner = () => (
+  <div className="banner">
+    <img src={Img} alt="Background" />
+  </div>
+);
 
 export default Banner;
