b40214074374f2f1d862e60da3f93c0b162d5eba
unknown <opokudavid446@gmail.com>
2020-06-04 13:43:23 +0100

    Modified banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,10 +1,27 @@
 import React from "react";
 import Img from "../images/img.jpg";
+import { Play } from "../svg/Svg";
 
 const Banner = () => (
   <div className="banner">
     <img src={Img} alt="Background" />
-    <div className="banner-container"></div>
+    <div className="banner-container">
+      <div className="section-1">
+        <p> Beauty </p>
+        <p>
+          is Power<span>.</span>
+        </p>
+      </div>
+      <hr />
+      <div className="section-2">
+        <p> The best part of beauty is that which </p>
+        <p> no picture can express </p>
+      </div>
+      <div className="section-3">
+        <Play size="15px" color="#ff6724" />
+        <p> Watch video </p>
+      </div>
+    </div>
   </div>
 );
 
